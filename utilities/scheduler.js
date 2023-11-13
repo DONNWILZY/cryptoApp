c8b560c30f37cf8887add360a4a175dd6ddf5b5b
donnwilzy <ask4godswill@gmail.com>
2023-11-14 00:44:41 +0100

    db

--- utilities/scheduler.js
+++ utilities/scheduler.js
@@ -1,5 +1,5 @@
 const cron = require('node-cron');
-const updateProfitAndMoveToWallet = require('../middleware/updateProfit');
+
 
 // Schedule to run every minute
 cron.schedule('*/1 * * * *', async () => {
