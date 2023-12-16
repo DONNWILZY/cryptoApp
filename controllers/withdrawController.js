d61bb91632f00bd788a942e41f4522c52832cff1
donnwilzy <ask4godswill@gmail.com>
2023-12-16 13:59:16 +0100

    withdrawal status added

--- controllers/withdrawController.js
+++ controllers/withdrawController.js
@@ -98,10 +98,82 @@ const withdraw = async (req, res) => {
 };
 
 
+const updateWithdrawal = async (req, res) => {
+  try {
+    const { withdrawalId, status, adminNotes } = req.body;
+
+    // Find the withdrawal by ID
+    const withdrawal = await Withdraw.findById(withdrawalId);
+
+    if (!withdrawal) {
+      return res.status(404).json({ error: 'Withdrawal not found' });
+    }
+
+    // Store the current status before updating
+    const currentStatus = withdrawal.status;
+
+    // Update the withdrawal status and adminNote
+    withdrawal.status = status;
+    if (adminNotes && Array.isArray(adminNotes)) {
+      withdrawal.adminNote.push(...adminNotes);
+    }
+
+    // Update user's wallet based on the status change
+    const user = await User.findById(withdrawal.user);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (currentStatus === 'pending' && status === 'approved') {
+      // Subtract the balance value if changing from pending to approved
+      if (withdrawal.withdrawalType === 'balanceToPersonalAccount') {
+        user.wallet.balance -= withdrawal.amount;
+        user.wallet.total -= withdrawal.amount;
+      }
+    } else if (currentStatus === 'approved' && status !== 'approved') {
+      // Add the balance value back if changing from approved to pending or others
+      if (withdrawal.withdrawalType === 'balanceToPersonalAccount') {
+        user.wallet.balance += withdrawal.amount;
+        user.wallet.total += withdrawal.amount;
+      }
+    }
+
+    // Save the updated withdrawal and user data
+    await withdrawal.save();
+    await user.save();
+
+    // Return updated withdrawal response data
+    const responseData = {
+      message: 'Withdrawal status updated successfully',
+      withdrawal: {
+        _id: withdrawal._id,
+        user: withdrawal.user,
+        amount: withdrawal.amount,
+        withdrawalType: withdrawal.withdrawalType,
+        status: withdrawal.status,
+        personalAccountCurrency: withdrawal.personalAccountCurrency,
+        personalAccountAddress: withdrawal.personalAccountAddress,
+        adminNote: withdrawal.adminNote,
+        createdAt: withdrawal.createdAt,
+        updatedAt: withdrawal.updatedAt,
+      },
+    };
+
+    res.status(200).json(responseData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+
+
 
 
 const withdrawController = {
-  withdraw
+  withdraw,
+  updateWithdrawal,
     
 };
 
--- routes/withdrawalRoute.js
+++ routes/withdrawalRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { withdraw } = require('../controllers/withdrawController');
+const { withdraw, updateWithdrawal } = require('../controllers/withdrawController');
 
 // Endpoint for handling withdrawals
 router.post('/withdraw', withdraw);
+
+// Route to update withdrawal status
+router.put('/update', updateWithdrawal);
 
 module.exports = router;
